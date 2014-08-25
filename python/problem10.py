3fe771ad5f4909b75a5fbeb04c5e4bc097820332
Michael Toth <mt.toth@gmail.com>
2014-08-24 22:55:12 -0400

    Relatively fast but could be improved.  Revisit.

---
+++ python/problem10.py
@@ -0,0 +1,17 @@
+# Function for finding the sum of all primes below n
+
+import math
+
+def sum_primes(num):
+     # Instantiate a true/false array with first 2 elements false and remaining true.
+     prime_bool = [False]*2 + [True]*(num-2)
+
+     for i in xrange(2, int(math.floor(math.sqrt(num) + 1))):
+          for j in xrange(i * 2, num, i):
+               prime_bool[j] = False
+               
+     # Convert boolean list to prime list using enumerate to get indices:
+
+     #prime_sum = sum([i for i, x in enumerate(prime_bool) if x == True])
+     prime_sum = reduce(lambda x, y: x+y, [i for i, x in enumerate(prime_bool) if x == True])
+     print prime_sum
