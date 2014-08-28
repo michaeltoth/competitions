199195d68cd19892342e348443669b972f648935
Michael Toth <mt.toth@gmail.com>
2014-08-28 14:32:45 -0400

    Solution with clever diagonal traversal implementation

--- python/problem11.py
+++ python/problem11.py
@@ -19,32 +19,30 @@ def get_grid(input_file):
 def get_column(matrix, i):
     return [row[i] for row in matrix]
 
-# Returns the max of all 4 digit products in columns of the grid
 def col_max(grid,num_cols):
     my_col_max = max([compute_max_product(get_column(grid,i)) for i in range(0,num_cols)])
     return my_col_max
 
-# Returns the max of all 4 digit products in rows of the grid
 def row_max(grid,num_rows):
     my_row_max = max([compute_max_product(grid[i]) for i in range(0,num_rows)])
     return my_row_max
 
-# Returns the max of all 4 digit products in diagonals of the grid
 def diag_max(grid,num_rows,num_cols):
-
+    # Loops through variables p and q where p is the number of the diagonal and q is the index along the diagonal.
+	# Converts p and q into (x,y) space for indexing from the grid in the pos_diags and neg_diags variables.
+	
+	# Up and right
     pos_diags = [[grid[q][p-q]
         for q in range(min(p, num_rows-1), max(0, p-num_cols+1)-1, -1)]
         for p in range(num_rows+num_cols-1)]	
+    # Down and left
     neg_diags = [[grid[num_rows-1-q][p-q]
         for q in range(min(p, num_rows-1), max(0, p-num_cols+1)-1, -1)]
         for p in range(num_rows+num_cols-1)]
 
-    # print [pos_diags[i] for i in range(0,num_rows+num_cols-1)]
+    # Compute and return the max of both positive and negative diagonal traversals:
     pos_traversal_max = max([compute_max_product(pos_diags[i]) for i in range(0,num_rows+num_cols-1)])
-	
-    # print [neg_diags[i] for i in range(0,num_rows+num_cols-1)]
     neg_traversal_max = max([compute_max_product(neg_diags[i]) for i in range(0,num_rows+num_cols-1)])
-	
     return max(pos_traversal_max,neg_traversal_max)
 	
 # Computes and returns the maximum 4 digit product of the passed vector
@@ -60,7 +58,6 @@ def compute_max_product(vector):
         end += 1
     return max
 
-# Receives input of file name and drives program, returning the max
 def main(input_file):
     my_grid = get_grid(input_file)
     n = len(my_grid)     # number of rows
