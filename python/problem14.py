a7c382f809063d2afe5cef83bada9dde269e8428
Michael Toth <mt.toth@gmail.com>
2014-09-02 19:25:29 -0400

    Much faster.  Executes in 2.5s

--- python/problem14.py
+++ python/problem14.py
@@ -1,13 +1,16 @@
 def collatz(num):
-	count = 1
-	while num > 1:
-		if num % 2 == 0:
-			num /= 2
-		else:
-			num = num*3 + 1
-		count += 1
-	return count
-
-
-def longest_collatz(num):
-	print max((i for i in range(num)), key = lambda x: collatz(x)) 
+	#Function to find the number with the maximum collatz sequence below the value num
+	collatz_array=[0,1]
+	for i in range(2,num + 1):
+		length = 0
+		# Quit once we already know the length of i and add this length
+		while i >= len(collatz_array):
+			if i % 2 == 0: i /= 2
+			else: i = i*3 + 1
+			length += 1
+		length += collatz_array[i]
+		collatz_array.append(length)
+	
+	# Printing the index of the maximal value
+	m = max(collatz_array)
+	print [i for i, j in enumerate(collatz_array) if j == m]
