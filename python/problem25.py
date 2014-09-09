e8430e4ed5b82a303835d6fba18b2fad51eb85a1
Michael Toth <mt.toth@gmail.com>
2014-09-09 17:14:24 -0400

    Converting tabs to spaces for clarity and standardization

--- python/problem25.py
+++ python/problem25.py
@@ -1,18 +1,18 @@
 def digits(n):
-	return len([char for char in str(n)])
+    return len([char for char in str(n)])
 
-def fibonacci(num_digits):
-	lastnum = 0
-	fib_num = 1
-	lastnum = 1
-	count = 2 	# Starting with 2 fibonacci numbers
+def get_fibonacci_n_digits(num_digits):
+    lastnum = 0
+    fib_num = 1
+    lastnum = 1
+    count = 2 	# Starting with 2 fibonacci numbers
 
-	while digits(fib_num) < num_digits:
-        	# Create temp variable for iteration
-        	temp = lastnum
-        	lastnum = fib_num
-        	fib_num += temp
-		count += 1
+    while digits(fib_num) < num_digits:
+        # Create temp variable for iteration
+        temp = lastnum
+        lastnum = fib_num
+        fib_num += temp
+        count += 1
 
-	print count, fib_num
+    print count, fib_num
 
