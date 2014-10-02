3642db03c6d2f0fe4e8424d0852819919b5a94a6
Michael Toth <mt.toth@gmail.com>
2014-10-02 19:53:34 -0400

    Set directory and load files

---
+++ titanic.R
@@ -0,0 +1,10 @@
+# Name: Michael Toth
+# Date: 10/1/2014
+# Summary: A script for analyzing the Titanic Kaggle competition data in R
+
+
+# Set working directory and load train and test files
+setwd("~/Documents/Projects/Kaggle/Titanic")
+train <- read.csv("~/Documents/Projects/Kaggle/Titanic/train.csv")
+test <- read.csv("~/Documents/Projects/Kaggle/Titanic/test.csv")
+
