0584113c2857b5620986ffa744328b3d3616a306
Michael Toth <mt.toth@gmail.com>
2014-10-02 20:34:25 -0400

    Females in 3rd class who pay 0+ also die

--- titanic.R
+++ titanic.R
@@ -18,3 +18,30 @@ test$Survived <- 0
 test$Survived[test$Sex == 'female'] <- 1
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "onlymendie.csv", row.names = FALSE)
+
+# Create categorical child variable to indicate passengers below 18
+train$Child <- 0
+train$Child[train$Age < 18] <- 1
+
+# Create a table of proportions who survived based on sex and child status
+aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+# Still we predict all men die and all women survive, regardless of age
+
+# Next let's create bins for fare values:
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+
+# Create another table based on sex, child status, and fare bucket
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, 
+          FUN=function(x) {sum(x)/length(x)})
+# This predicts women in 3rd class who pay 20-30 or 30+ dollars are more likely 
+# to die.  This may not make sense, but we can try it out
+
+# Predict all men die, and females in 3rd class with tickets more than $20 die
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "femalesclassfare.csv", row.names = FALSE)
